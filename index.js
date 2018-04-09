78ddae94728eef8143f214aea4316e1f32484b78
skbolton <stephenkbolton@gmail.com>
2018-04-09 02:31:32 -0600

    initial docs

--- index.js
+++
@@ -1,8 +0,0 @@
-const Arbiter = require('./lib/arbiter')
-const Schema = require('./lib/schema/schema')
-const Model = require('./lib/model')
-const Connection = require('./lib/connection')
-
-const arbiter = new Arbiter(Connection, Model)
-arbiter.Schema = Schema
-module.exports = arbiter
